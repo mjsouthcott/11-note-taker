d9eab3f60b7a18bc235ef5a4e274de7eea4e795c
Matthew Southcott <mjsouthcott@gmail.com>
2020-04-22 18:35:09 -0400

    Add POST functionality

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Basic GET functionality
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
@@ -13,16 +16,33 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 app.get("/api/notes", (req, res) => {
-  fs.readFile(__dirname + "/db/db.json", "utf8", function (err, data) {
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
     if (err) throw err;
-    return res.json(JSON.parse(data));
+    const notes = JSON.parse(data);
+    return res.json(notes);
   });
 });
 
-// TODO: Basic POST functionality
+// Basic POST functionality
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile(
+      __dirname + "/db/db.json",
+      JSON.stringify(notes),
+      (err, data) => {
+        if (err) throw err;
+      }
+    );
+  });
+});
 
 // TODO: Basic DELTE functionality
 
+// Handle 404 errors by sending index.html
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "public/index.html"));
 });
