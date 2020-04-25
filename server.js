4765787d5cd5717933eeb905ca3789f3f4b9e8df
Matthew Southcott <mjsouthcott@gmail.com>
2020-04-25 17:52:09 -0400

    Make notes.html display properly and fix DELETE functionality

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
-// Basic GET functionality
+// Define app GET methods that serve web content and provide API endpoint
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
@@ -23,43 +25,40 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
-// Basic POST functionality
+// Define app POST method to add new notes at API endpoint
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
   fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     const notes = JSON.parse(data);
+
+    // Create and assign unique note id
+    newNote.id = uuidv4();
     notes.push(newNote);
-    fs.writeFile(
-      __dirname + "/db/db.json",
-      JSON.stringify(notes),
-      (err, data) => {
-        if (err) throw err;
-      }
-    );
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      else return res.json(notes);
+    });
   });
 });
 
-// Basic DELETE functionality
+// Define app DELETE method to delete notes by id at API endpoint
 app.delete("/api/notes/:id", (req, res) => {
-  const id = req.params.id - 1;
   fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     const notes = JSON.parse(data);
-    notes.splice(id, 1);
-    fs.writeFile(
-      __dirname + "/db/db.json",
-      JSON.stringify(notes),
-      (err, data) => {
-        if (err) throw err;
-      }
-    );
+    notes.splice(req.params.id - 1, 1);
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      else return res.json(notes);
+    });
   });
 });
 
-// Handle 404 errors by sending index.html
+// Handle 404 errors by serving index.html
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "public/index.html"));
 });
 
+// Run app
 app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}.`));
